251cc06ce8a763f8327bec01a93354d503ed4e7f
Mike Linnen <mlinnen@protosystem.net>
2012-11-18 18:09:50 -0500

    Added the functionality for the Alarm device

--- src/HomeSecurity.Device.Alarm/HomeSecurity.Device.Alarm/AlarmController.cs
+++ src/HomeSecurity.Device.Alarm/HomeSecurity.Device.Alarm/AlarmController.cs
@@ -2,6 +2,9 @@ using System;
 using Microsoft.SPOT;
 using MQTT;
 using Device.Core;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using System.Threading;
 
 namespace HomeSecurity.Device.ExternalDoor
 {
@@ -11,6 +14,32 @@ namespace HomeSecurity.Device.ExternalDoor
         private readonly ILogger _logger;
         private string _locationCode;
         private string _houseCode;
+        private const string _masterBedroomBurglarTopic = "/alarmpanel/masterbedroom/burglar";
+        private const string _bedroom1BurglarTopic = "/alarmpanel/bedroom1/burglar";
+        private const string _bedroom2BurglarTopic = "/alarmpanel/bedroom2/burglar";
+        private const string _firstFloorBurglarTopic = "/alarmpanel/firstfloor/burglar";
+        private const string _masterBedroomWindowTopic = "/alarmpanel/masterbedroom/window";
+        private const string _bedroom1WindowTopic = "/alarmpanel/bedroom1/window";
+        private const string _bedroom2WindowTopic = "/alarmpanel/bedroom2/window";
+        private const string _firstFloorWindowTopic = "/alarmpanel/firstfloor/window";
+        private const string _firstFloorMotionTopic = "/alarmpanel/firstfloor/motion";
+        private const string _frontDoorTopic = "/externaldoor/front/door";
+        private const string _sideDoorTopic = "/externaldoor/side/door";
+        private const string _backDoorTopic = "/externaldoor/back/door";
+        private bool _masterBedroomBurglarAlarmOn = false;
+        private bool _bedroom1BurglarAlarmOn = false;
+        private bool _bedroom2BurglarAlarmOn = false;
+        private bool _firsFloorBurglarAlarmOn = false;
+        private Timer _burglarAlarmTimer = null;
+        private OutputPort _burglarAlarmOutput = new OutputPort(Pins.GPIO_PIN_D0, false);
+        private OutputPort _masterBedroomWindowOutput = new OutputPort(Pins.GPIO_PIN_D1, false);
+        private OutputPort _bedroom1WindowOutput = new OutputPort(Pins.GPIO_PIN_D2, false);
+        private OutputPort _bedroom2WindowOutput = new OutputPort(Pins.GPIO_PIN_D3, false);
+        private OutputPort _firstFloorWindowOutput = new OutputPort(Pins.GPIO_PIN_D4, false);
+        private OutputPort _firstFloorMotionOutput = new OutputPort(Pins.GPIO_PIN_D5, false);
+        private OutputPort _frontDoorOutput = new OutputPort(Pins.GPIO_PIN_D6, false);
+        private OutputPort _backDoorOutput = new OutputPort(Pins.GPIO_PIN_D7, false);
+        private OutputPort _sideDoorOutput = new OutputPort(Pins.GPIO_PIN_D8, false);
 
 		#region ctor
 
@@ -20,6 +49,9 @@ namespace HomeSecurity.Device.ExternalDoor
             _mqttService = mqttService;
 			_houseCode = houseCode;
             _locationCode = locationCode;
+
+            // Setup the timer to wait forever
+            _burglarAlarmTimer = new Timer(new TimerCallback(OnTimer), this._burglarAlarmOutput, Timeout.Infinite, Timeout.Infinite);
         }
 
 		#endregion
@@ -41,7 +73,6 @@ namespace HomeSecurity.Device.ExternalDoor
         {
 			if (ConnectToBroker()){
 				if (Subscribe()){
-					// TODO add the logic to handle the I/O
 
                     // Send out a ping topic with Hello World as the message and it should come back to this device as a pingresp
                     _mqttService.Publish(new MqttParcel(Topic + "ping","Hello world",QoS.BestEfforts,false));
@@ -83,8 +114,30 @@ namespace HomeSecurity.Device.ExternalDoor
 
             try
             {
-				Subscription subscription = new Subscription(Topic + "pingresp", QoS.BestEfforts);
+                Subscription subscription = null; ;
+                subscription = new Subscription(Topic + "pingresp", QoS.BestEfforts);
                 messageId = _mqttService.Subscribe(subscription);
+
+                // Subscribe to any alarm panel burglar messages
+                string topic = "/" + _houseCode + "/alarmpanel/+/burglar";
+                subscription = new Subscription(topic, QoS.BestEfforts);
+                messageId = _mqttService.Subscribe(subscription);
+
+                // Subscribe to any alarm panel window messages
+                topic = "/" + _houseCode + "/alarmpanel/+/window";
+                subscription = new Subscription(topic, QoS.BestEfforts);
+                messageId = _mqttService.Subscribe(subscription);
+
+                // Subscribe to any alarm panel motion messages
+                topic = "/" + _houseCode + "/alarmpanel/+/motion";
+                subscription = new Subscription(topic, QoS.BestEfforts);
+                messageId = _mqttService.Subscribe(subscription);
+
+                // Subscribe to any external door messages
+                topic = "/" + _houseCode + "/externaldoor/+/door";
+                subscription = new Subscription(topic, QoS.BestEfforts);
+                messageId = _mqttService.Subscribe(subscription);
+
                 success = true;
             }
             catch (Exception ex)
@@ -109,10 +162,147 @@ namespace HomeSecurity.Device.ExternalDoor
 				return true;
             }
 
-			// TODO test for more subscriptions arriving and execute on them
+            CheckForBurglarMessages(e);
+
+            CheckForWindowMessages(e);
+
+            CheckForDoorMessages(e);
+
+            CheckForMotionMessages(e);
 
             return true;
 		}
+
+        public void CheckForBurglarMessages(PublishArrivedArgs e)
+        {
+            if (e.Topic.Equals("/" + _houseCode + _masterBedroomBurglarTopic))
+            {
+                _masterBedroomBurglarAlarmOn = ParseBurglarMessageValue(e.Payload);
+
+                SetBurglarAlarmOutput();
+            }
+
+            if (e.Topic.Equals("/" + _houseCode + _bedroom1BurglarTopic))
+            {
+                _bedroom1BurglarAlarmOn = ParseBurglarMessageValue(e.Payload);
+
+                SetBurglarAlarmOutput();
+            }
+
+            if (e.Topic.Equals("/" + _houseCode + _bedroom2BurglarTopic))
+            {
+                _bedroom2BurglarAlarmOn = ParseBurglarMessageValue(e.Payload);
+
+                SetBurglarAlarmOutput();
+            }
+
+            if (e.Topic.Equals("/" + _houseCode + _firstFloorBurglarTopic))
+            {
+                _firsFloorBurglarAlarmOn = ParseBurglarMessageValue(e.Payload);
+
+                SetBurglarAlarmOutput();
+            }
+        }
+
+        public void CheckForWindowMessages(PublishArrivedArgs e)
+        {
+            if (e.Topic.Equals("/" + _houseCode + _masterBedroomWindowTopic))
+            {
+                _masterBedroomWindowOutput.Write(ParseSensorMessageValue(e.Payload));
+            }
+
+            if (e.Topic.Equals("/" + _houseCode + _bedroom1WindowTopic))
+            {
+                _bedroom1WindowOutput.Write(ParseSensorMessageValue(e.Payload));
+            }
+
+            if (e.Topic.Equals("/" + _houseCode + _bedroom2WindowTopic))
+            {
+                _bedroom2WindowOutput.Write(ParseSensorMessageValue(e.Payload));
+            }
+
+            if (e.Topic.Equals("/" + _houseCode + _firstFloorWindowTopic))
+            {
+                _firstFloorWindowOutput.Write(ParseSensorMessageValue(e.Payload));
+            }
+        }
+
+        public void CheckForDoorMessages(PublishArrivedArgs e)
+        {
+            if (e.Topic.Equals("/" + _houseCode + _frontDoorTopic))
+            {
+                _frontDoorOutput.Write(ParseSensorMessageValue(e.Payload));
+            }
+
+            if (e.Topic.Equals("/" + _houseCode + _backDoorTopic))
+            {
+                _backDoorOutput.Write(ParseSensorMessageValue(e.Payload));
+            }
+
+            if (e.Topic.Equals("/" + _houseCode + _sideDoorTopic))
+            {
+                _sideDoorOutput.Write(ParseSensorMessageValue(e.Payload));
+            }
+
+        }
+
+        public void CheckForMotionMessages(PublishArrivedArgs e)
+        {
+            if (e.Topic.Equals("/" + _houseCode + _firstFloorMotionTopic))
+            {
+                _firstFloorMotionOutput.Write(ParseSensorMessageValue(e.Payload));
+            }
+        }
+
+        public void SetBurglarAlarmOutput()
+        {
+            if (_masterBedroomBurglarAlarmOn || _bedroom1BurglarAlarmOn || _bedroom2BurglarAlarmOn || _firsFloorBurglarAlarmOn)
+            {
+                _burglarAlarmTimer.Change(0, 2000);
+            }
+            else
+            {
+                _burglarAlarmTimer.Change(Timeout.Infinite, Timeout.Infinite);
+                _burglarAlarmOutput.Write(false);
+            }
+        }
+
+        private bool ParseBurglarMessageValue(MqttPayload payload)
+        {
+            bool returnValue = false;
+
+            if (payload.ToString().Equals("on"))
+            {
+                returnValue = true;
+            }
+            else if (payload.ToString().Equals("off"))
+            {
+                returnValue = false;
+            }
+            return returnValue;
+        }
+
+        private bool ParseSensorMessageValue(MqttPayload payload)
+        {
+            bool returnValue = false;
+
+            if (payload.ToString().Equals("opened"))
+            {
+                returnValue = true;
+            }
+            else if (payload.ToString().Equals("closed"))
+            {
+                returnValue = false;
+            }
+            return returnValue;
+        }
+
+        private static void OnTimer(object state)
+        {
+            OutputPort output = (OutputPort)state;
+            bool isOn = output.Read();
+            output.Write(!isOn);
+        }
 
 		#endregion
 	}
